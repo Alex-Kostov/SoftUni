44ee5bbb963269d43cfcecbba0d304d2f1fc0584
Alex-Kostov <alex89773@gmail.com>
2021-01-18 11:05:01 +0200

    Update 09.magicMatrices.js

--- JS-Advanced/02.Arrays And Nested Arrays/09.magicMatrices.js
+++ JS-Advanced/02.Arrays And Nested Arrays/09.magicMatrices.js
@@ -10,14 +10,12 @@ function solve(inputMatrice) {
   for (let i = 0; i < inputMatrice.length; i++) {
     let tempArr = [];
     for (let y = 0; y < inputMatrice.length; y++) {
-        tempArr.push(inputMatrice[y][i]);
+      tempArr.push(inputMatrice[y][i]);
     }
-    results.push(tempArr.reduce((acc,curr)=> acc + curr));
+    results.push(tempArr.reduce((acc, curr) => acc + curr));
   }
-  console.log(results.every((x) => x === results[0])); 
+  console.log(results.every((x) => x === results[0]));
 }
-solve(   [[1, 0, 0],
-  [0, 0, 1],
-  [0, 1, 0]]
- 
-  );
+solve([[1, 0, 0],
+[0, 0, 1],
+[0, 1, 0]]);
